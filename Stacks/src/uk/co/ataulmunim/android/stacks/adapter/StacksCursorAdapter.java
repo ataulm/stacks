33bb623123da51aabf5426729adee660e20057db
Ataul Munim <timberwo7ves@gmail.com>
2013-03-07 19:50:02 +0000

    Add missing files

---
+++ Stacks/src/uk/co/ataulmunim/android/stacks/adapter/StacksCursorAdapter.java
@@ -0,0 +1,96 @@
+package uk.co.ataulmunim.android.stacks.adapter;
+
+import uk.co.ataulmunim.android.stacks.contentprovider.Stacks;
+
+import com.nicedistractions.shortstacks.R;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class StacksCursorAdapter extends SimpleCursorAdapter {
+
+	public static final String LOG_TAG = "StacksCursorAdapter";
+	/**
+	 * Saves Plans data to avoid requerying the content provider when the user scrolls the list.
+	 */
+	private SparseArray<String> cachedPlans;
+	
+	/**
+	 * Standard constructor, with additional flag to register content observer.
+	 * 
+	 * @param context
+	 * @param layout
+	 * @param c
+	 * @param from
+	 * @param to
+	 */
+	public StacksCursorAdapter(Context context, int layout, Cursor c, String[] from, int[] to) {
+		super(context, layout, c, from, to, 0);
+	}
+	
+	/**
+	 * Get a View that displays the data at the specified position in the data set.
+	 * In this override, the 
+	 */
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View row = super.getView(position, convertView, parent);
+		ViewHolder holder = (ViewHolder) row.getTag();
+		
+		int stackId = getCursor().getInt(getCursor().getColumnIndex(Stacks._ID));		
+		
+		if (holder == null) {
+			holder = new ViewHolder(row);
+			row.setTag(holder);
+		}
+		
+		// Actionable items
+		if (holder.actionItems.getText().toString() != "" &&
+				Integer.parseInt(holder.actionItems.getText().toString()) < 1) {
+			holder.actionItems.setVisibility(View.GONE);
+		}
+		else holder.actionItems.setVisibility(View.VISIBLE);
+		
+		// Plans
+		if (cachedPlans != null && cachedPlans.get(stackId) != null) {
+			holder.plans.setText(cachedPlans.get(stackId));
+		}
+		else holder.plans.setText("");
+		
+		return row;
+	}
+	
+	/**
+	 * Updates the cache of Plans from the Fragment so
+	 * {@link StacksCursorAdapter#getView(int, View, ViewGroup)} can update rows with plans.
+	 * 
+	 * @param cachedPlans
+	 */
+	public void setCachedPlans(SparseArray<String> cachedPlans) {
+		this.cachedPlans = cachedPlans;
+	}
+	
+    /**
+     * The ViewHolder pattern allows reduction in number of findViewById() calls we need to do
+     * @author ataulm
+     *
+     */
+    class ViewHolder {
+    	TextView plans = null;
+    	TextView actionItems = null;
+    	
+    	ViewHolder(View row) {
+    		this.plans = (TextView) row.findViewById(R.id.listitem_plans);
+    		this.actionItems = (TextView) row.findViewById(R.id.listitem_actionable_items);
+    	}
+    }
+}
---
+++ Stacks/src/uk/co/ataulmunim/android/stacks/fragment/StacksEditFragment.java
@@ -0,0 +1,186 @@
+package uk.co.ataulmunim.android.stacks.fragment;
+import uk.co.ataulmunim.android.stacks.Crud;
+import uk.co.ataulmunim.android.stacks.adapter.StacksCursorAdapter;
+import uk.co.ataulmunim.android.stacks.contentprovider.Plans;
+import uk.co.ataulmunim.android.stacks.contentprovider.Stacks;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.util.Log;
+import android.util.SparseArray;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockListFragment;
+import com.nicedistractions.shortstacks.R;
+
+
+public class StacksEditFragment extends SherlockFragment
+	implements LoaderManager.LoaderCallbacks<Cursor> {
+	
+	public static final String LOG_TAG = "StacksListFragment";
+	
+	public static final String[] STACKS_PROJECTION = {
+		Stacks._ID,	Stacks.NAME, Stacks.ACTION_ITEMS
+	};
+	
+	public static final String[] PLANS_PROJECTION = {
+		Plans._ID, Plans.DAY, Plans.STACK
+	};
+	
+	public static final int STACKS_LOADER = 0;
+	public static final int DATES_LOADER = 1;
+	public static final int PLANS_LOADER = 2;
+	
+	/**
+	 * Determines whether or not to close the soft input keyboard when adding Stacks to the list.
+	 * A value of TRUE will leave it open, but this can be set in shared preferences.
+	 */
+	private boolean quickAddMode;
+	
+	private StacksCursorAdapter adapter;
+	private int stackId = Stacks.ROOT_STACK_ID; // id of the current stack in the Stacks table
+	
+	public static StacksEditFragment newInstance() {
+		return new StacksEditFragment();
+    }
+	
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_stack_edit, container, false);
+    }
+	
+	
+	
+	/**
+	 * Called after onCreateView(), after the parent activity is created
+	 */
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		Log.i(LOG_TAG, "onActivityCreated()");
+		
+		final Intent intent = getActivity().getIntent();
+		final Uri stackUri = intent.getData();
+		
+		// TODO: differentiate between INTENTs by performing different actions
+		final String action = intent.getAction();
+		
+		if (stackUri != null) {
+			try {
+				stackId = Integer.parseInt(stackUri.getLastPathSegment());	
+			} catch (NumberFormatException e) {
+				Log.w(LOG_TAG, "stackUri.getLastPathSegment() was not cool. (" +
+						stackUri.getLastPathSegment() + ")." +
+						"Stays unchanged as Stacks.ROOT_STACK_ID.");
+			}	
+		}
+        
+        // Prepare the loader.  Either re-connect with an existing one, or start a new one.
+        //getActivity().getSupportLoaderManager().initLoader(STACKS_LOADER, null, this);
+        //getActivity().getSupportLoaderManager().initLoader(PLANS_LOADER, null, this);
+	}
+	
+	
+	// Loaders ////////////////////////////////////////////////////////////////////////////////////
+
+	@Override
+	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+		CursorLoader cursorLoader = null;
+		
+		if (id == STACKS_LOADER) {
+			Log.d(LOG_TAG, "Loading stacks under stack " + stackId);
+			final String where = Stacks.PARENT + "=" + stackId +
+					" AND " + Stacks.DELETED + "<> 1" + " AND " + 
+					Stacks._ID + "<>" + Stacks.ROOT_STACK_ID; // Don't show default stack as child
+			
+			cursorLoader = new CursorLoader(getActivity(),
+					Stacks.CONTENT_URI,
+					STACKS_PROJECTION,
+					where,
+					null,
+					Stacks.LOCAL_SORT);
+		}
+		
+		else if (id == PLANS_LOADER ) {
+			Log.d(LOG_TAG, "Loading plans for stacks under stack " + stackId);
+			
+			final String where = Plans.VALUE + "> 0";
+			// URI for all plans which are connected to the Stacks table
+			final Uri allPlans = Stacks.PLANS.getAll(Stacks.CONTENT_URI);
+			
+			cursorLoader = new CursorLoader(getActivity(),
+					allPlans,
+					PLANS_PROJECTION,
+					where,
+					null,
+					Plans.STACK);
+		}
+		
+		return cursorLoader;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+		if (loader.getId() == STACKS_LOADER) {
+			Log.d(LOG_TAG, "Stacks loaded, swapping cursor, scrolling to end.");
+			
+			adapter.swapCursor(data);
+		}
+		else if (loader.getId() == PLANS_LOADER) {
+			Log.d(LOG_TAG, data.getCount() + " plans loaded");
+			SparseArray<String> plans = new SparseArray<String>();
+			
+			/**
+			 * The idea is to append. if empty add. if not empty append.
+			 */
+			if (data.moveToFirst()) {
+				do {
+					final int stack = data.getInt(data.getColumnIndex(Plans.STACK));
+					final String plan = plans.get(stack);
+					final int dayCode = data.getInt(data.getColumnIndex(Plans.DAY)); 
+					final String day = Plans.getDayShort(dayCode);
+					
+					if (plan == null) {
+						plans.put(stack, day);
+					} else if (!plan.contains(day)) {
+						plans.put(stack, plan + " " + day); 
+					}
+					Log.d(LOG_TAG, "key: "+ stack+ ", plan: "+ plans.get(stack));
+				} while (data.moveToNext());
+			}
+			
+			// Update views already visible
+			adapter.setCachedPlans(plans);
+			adapter.notifyDataSetChanged();			
+		}
+	}	
+	
+	@Override
+	public void onLoaderReset(Loader<Cursor> loader) {
+		if (loader.getId() == STACKS_LOADER) {
+			Log.d(LOG_TAG, "Closing last Stacks cursor, so setting adapter cursor to null.");
+			adapter.swapCursor(null);
+		}
+	}
+	
+	// Loaders end ////////////////////////////////////////////////////////////////////////////////
+}
