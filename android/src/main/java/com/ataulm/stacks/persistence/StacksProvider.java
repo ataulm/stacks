28b960ec2a6e44bcf08b67f04f36e649583d9340
Ataul Munim <timberwo7ves@gmail.com>
2014-02-22 17:16:41 +0000

    Adds debug and release build type with authorities

---
+++ android/src/main/java/com/ataulm/stacks/persistence/StacksProvider.java
@@ -0,0 +1,18 @@
+package com.ataulm.stacks.persistence;
+
+import android.net.Uri;
+
+import com.ataulm.stacks.BuildConfig;
+
+import novoda.lib.sqliteprovider.provider.SQLiteContentProviderImpl;
+
+public class StacksProvider extends SQLiteContentProviderImpl {
+
+    public static final Uri AUTHORITY = Uri.parse("content://" + BuildConfig.PROVIDER_AUTHORITY);
+    public static final String MIME_STACK = "vnd.android.cursor.item/vnd.com.ataulm.stack";
+
+    public static Uri uriFrom(Table table) {
+            return AUTHORITY.buildUpon().appendPath(table.toString()).build();
+    }
+
+}
---
+++ android/src/main/java/com/ataulm/stacks/persistence/Table.java
@@ -0,0 +1,11 @@
+package com.ataulm.stacks.persistence;
+
+public enum Table {
+
+    STACKS;
+
+    public String toString() {
+        return super.toString().toLowerCase();
+    }
+
+}
