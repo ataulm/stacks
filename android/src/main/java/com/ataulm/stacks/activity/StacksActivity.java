a6f23d02e8127851c74ac5aa888758d8fc937cf2
Ataul Munim <timberwo7ves@gmail.com>
2014-02-22 14:28:23 +0000

    Compilable deletion of everything

--- android/src/main/java/com/ataulm/stacks/activity/StacksActivity.java
+++ android/src/main/java/com/ataulm/stacks/activity/StacksActivity.java
@@ -1,8 +1,16 @@
 package com.ataulm.stacks.activity;
 
+import android.os.Bundle;
+
 import com.ataulm.stacks.base.BaseActivity;
+import com.ataulm.stacks.R;
 
 
 public class StacksActivity extends BaseActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stacks);
+    }
 }
