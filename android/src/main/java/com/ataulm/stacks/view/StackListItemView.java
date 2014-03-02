7c2a2b7b761e6eb8baf9abdb55710acd5d002bbc
Ataul Munim <timberwo7ves@gmail.com>
2014-03-02 16:05:00 +0000

    Adds typefacefactory and custom textview

--- android/src/main/java/com/ataulm/stacks/view/StackListItemView.java
+++ android/src/main/java/com/ataulm/stacks/view/StackListItemView.java
@@ -2,18 +2,29 @@ package com.ataulm.stacks.view;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.ataulm.stacks.R;
 import com.ataulm.stacks.model.Stack;
+import com.novoda.notils.caster.Views;
 
-public class StackListItemView extends TextView {
+public class StackListItemView extends RelativeLayout {
+
+    private TextView textViewSummary;
 
     public StackListItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        textViewSummary = Views.findById(this, R.id.textview_summary);
+    }
+
     public void updateWith(Stack stack) {
-        setText(stack.summary);
+        textViewSummary.setText(stack.summary);
     }
 
 }
