eb9837ac84ee8079ac9bc4a6b7efbc86fe95808b
Ataul Munim <hello@ataulm.com>
2016-03-17 22:08:01 +0000

    Add Optional implementation

---
+++ core/src/main/java/com/ataulm/stacks/Optional.java
@@ -0,0 +1,69 @@
+package com.ataulm.stacks;
+
+public final class Optional<T> {
+
+    @SuppressWarnings("unchecked")
+    private static final Optional ABSENT = new Optional(null);
+
+    private final T data;
+
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<T> absent() {
+        return ABSENT;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<T> from(T data) {
+        if (data == null) {
+            return ABSENT;
+        }
+        return new Optional<>(data);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> Optional<T> of(T data) {
+        if (data == null) {
+            throw new IllegalArgumentException("Data cannot be null. Use Optional.from(maybeNullData).");
+        }
+        return new Optional<>(data);
+    }
+
+    private Optional(T data) {
+        this.data = data;
+    }
+
+    public boolean isPresent() {
+        return data != null;
+    }
+
+    public T get() {
+        if (!isPresent()) {
+            throw new IllegalArgumentException("You must check if data is present before using get()");
+        }
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Optional<?> optional = (Optional<?>) o;
+
+        if (data != null ? !data.equals(optional.data) : optional.data != null) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return data != null ? data.hashCode() : 0;
+    }
+
+}
