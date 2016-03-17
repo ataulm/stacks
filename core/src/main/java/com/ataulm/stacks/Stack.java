0bc8bfbd4fc29db801c73ab458192d11658b75ec
Ataul Munim <hello@ataulm.com>
2016-03-17 22:15:40 +0000

    Add Stack data class

---
+++ core/src/main/java/com/ataulm/stacks/Stack.java
@@ -0,0 +1,21 @@
+package com.ataulm.stacks;
+
+import com.google.auto.value.AutoValue;
+
+@AutoValue
+public abstract class Stack {
+
+    public abstract String id();
+    public abstract String summary();
+    public abstract Optional<String> parentId();
+    public abstract Optional<String> description();
+
+    public static Stack create(String id, String summary, Optional<String> parentId, Optional<String> description) {
+        return new AutoValue_Stack(id, summary, parentId, description);
+    }
+
+    Stack() {
+        // instantiate AutoValue generated class
+    }
+
+}
