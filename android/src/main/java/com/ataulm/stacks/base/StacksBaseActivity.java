9a8c8a64400edcd26a6e37c439d2ee9864e27d6d
Ataul Munim <hello@ataulm.com>
2016-03-18 08:48:21 +0000

    Remove viewserver

--- android/src/main/java/com/ataulm/stacks/base/StacksBaseActivity.java
+++ android/src/main/java/com/ataulm/stacks/base/StacksBaseActivity.java
@@ -6,28 +6,13 @@ import android.widget.Toast;
 
 public class StacksBaseActivity extends Activity {
 
-    private ViewServerManager viewServerManager;
     private Navigator navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        viewServerManager = new ViewServerManager(this);
-        viewServerManager.onCreate();
         navigator = new Navigator(this);
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        viewServerManager.onResume();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        viewServerManager.onDestroy();
     }
 
     protected Navigator navigateTo() {
