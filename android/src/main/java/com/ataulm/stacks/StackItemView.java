e15d5e1909a67003444a22677603cb9b22498892
Ataul Munim <hello@ataulm.com>
2016-03-28 11:49:58 +0100

    Bind a11y actions to StackItemView

--- android/src/main/java/com/ataulm/stacks/StackItemView.java
+++ android/src/main/java/com/ataulm/stacks/StackItemView.java
@@ -1,12 +1,21 @@
 package com.ataulm.stacks;
 
 import android.content.Context;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.app.AlertDialog;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ataulm.stacks.stack.Stack;
+import com.novoda.accessibility.AccessibilityServices;
+import com.novoda.accessibility.Action;
+import com.novoda.accessibility.Actions;
+import com.novoda.accessibility.ActionsAccessibilityDelegate;
+import com.novoda.accessibility.ActionsAlertDialogCreator;
+
+import java.util.Arrays;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,8 +31,11 @@ public class StackItemView extends LinearLayout {
     @Bind(R.id.stack_item_button_remove)
     View removeButton;
 
+    private final AccessibilityServices accessibilityServices;
+
     public StackItemView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.accessibilityServices = AccessibilityServices.newInstance(getContext());
         setOrientation(HORIZONTAL);
     }
 
@@ -34,8 +46,73 @@ public class StackItemView extends LinearLayout {
         ButterKnife.bind(this);
     }
 
-    public void bind(final Stack stack, final StackItemListener listener) {
+    public void bind(Stack stack, StackItemListener listener) {
+        setContentDescription(stack.summary());
         summaryTextView.setText(stack.summary());
+
+        Actions actions = createActions(stack, listener);
+        final AlertDialog alertDialog = new ActionsAlertDialogCreator(getContext(), R.string.stack_actions_title, actions).create();
+
+        if (accessibilityServices.isSpokenFeedbackEnabled()) {
+            ViewCompat.setAccessibilityDelegate(this, new ActionsAccessibilityDelegate(getResources(), actions));
+            addOnClickToShow(alertDialog);
+        } else {
+            addIndividualInteractions(stack, listener);
+            addOnLongClickToShow(alertDialog);
+        }
+    }
+
+    private static Actions createActions(Stack stack, StackItemListener listener) {
+        return new Actions(Arrays.asList(
+                createViewActionFor(stack, listener),
+                createEditActionFor(stack, listener),
+                createRemoveActionFor(stack, listener)
+        ));
+    }
+
+    private static Action createViewActionFor(final Stack stack, final StackItemListener listener) {
+        return new Action(R.id.stack_item_action_view, R.string.stack_item_view, new Runnable() {
+            @Override
+            public void run() {
+                listener.onClick(stack);
+            }
+        });
+    }
+
+    private static Action createRemoveActionFor(final Stack stack, final StackItemListener listener) {
+        return new Action(R.id.stack_item_action_remove, R.string.stack_item_remove, new Runnable() {
+            @Override
+            public void run() {
+                listener.onClickRemove(stack);
+            }
+        });
+    }
+
+    private static Action createEditActionFor(final Stack stack, final StackItemListener listener) {
+        return new Action(R.id.stack_item_action_edit, R.string.stack_item_edit, new Runnable() {
+            @Override
+            public void run() {
+                listener.onClickEdit(stack);
+            }
+        });
+    }
+
+    private void addOnClickToShow(final AlertDialog alertDialog) {
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.show();
+            }
+        });
+    }
+
+    private void addIndividualInteractions(final Stack stack, final StackItemListener listener) {
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onClick(stack);
+            }
+        });
 
         removeButton.setOnClickListener(new OnClickListener() {
             @Override
@@ -50,10 +127,14 @@ public class StackItemView extends LinearLayout {
                 listener.onClickEdit(stack);
             }
         });
-        setOnClickListener(new OnClickListener() {
+    }
+
+    private void addOnLongClickToShow(final AlertDialog alertDialog) {
+        setOnLongClickListener(new OnLongClickListener() {
             @Override
-            public void onClick(View v) {
-                listener.onClick(stack);
+            public boolean onLongClick(View v) {
+                alertDialog.show();
+                return true;
             }
         });
     }
