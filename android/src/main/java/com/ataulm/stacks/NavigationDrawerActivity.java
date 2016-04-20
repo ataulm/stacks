7657364223fd58999f7b2b3d843d74f397bf2245
Ataul Munim <hello@ataulm.com>
2016-04-20 21:01:42 +0100

    Add empty navigation drawer

--- android/src/main/java/com/ataulm/stacks/NavigationDrawerActivity.java
+++ android/src/main/java/com/ataulm/stacks/NavigationDrawerActivity.java
@@ -1,4 +1,18 @@
 package com.ataulm.stacks;
 
+import android.support.annotation.LayoutRes;
+import android.view.View;
+import android.view.ViewGroup;
+
 public abstract class NavigationDrawerActivity extends BaseActivity {
+
+    @Override
+    public void setContentView(@LayoutRes int layout) {
+        super.setContentView(R.layout.activity_navigation_drawer);
+        ViewGroup content = (ViewGroup) findViewById(R.id.content);
+        getLayoutInflater().inflate(layout, content);
+
+        View drawerView = findViewById(R.id.drawer);
+    }
+
 }
