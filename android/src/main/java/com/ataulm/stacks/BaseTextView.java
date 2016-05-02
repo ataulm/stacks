a380c0ad234681d73a80157bbb99db3fa8eca2a1
Ataul Munim <hello@ataulm.com>
2016-05-02 20:19:39 +0100

    Add BaseTextView
    - should start to use this before we get too many textviews
    - there are fixes that need applying to tv, and we can set a font on our one

---
+++ android/src/main/java/com/ataulm/stacks/BaseTextView.java
@@ -0,0 +1,13 @@
+package com.ataulm.stacks;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class BaseTextView extends TextView {
+
+    public BaseTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
